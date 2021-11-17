a62539ae6cd5dc6cd30d1108f40319e83caf3d45
Henrique Mazzoleni <henrique.mazzoleni@gmail.com>
2021-11-17 15:52:53 +0000

    variables

--- index.php
+++ index.php
@@ -8,7 +8,29 @@
         echo $username;
         echo "<br>";
         $current_user = $username;
-        echo $current_user
+        echo $current_user;
+        echo "<br>";
+        $count = 14;
+        echo $count;
+        echo "<br>";
+        $count = 17.2;
+        echo $count;
+        echo "<br>";
+        $familia = array('Henrique', 'Annia', 'Gabriella');
+        echo $familia[1];
+        echo "<br>";
+        echo 2 + 5;
+        echo "<br>";
+        echo 7%2;
+        echo "<br>";
+        $count += 1;
+        ++$count;
+        echo $count;
+        echo "<br>";
+        echo 5 . 2;
+        echo "<br>";
+        echo $count.=2;
+        echo true == true and false;
     ?> 
   </body>
 </html>
