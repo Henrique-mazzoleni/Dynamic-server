ca99f654f729d49315994f4b77d90f67f1631d81
Henrique Mazzoleni <henrique.mazzoleni@gmail.com>
2021-11-18 12:12:38 +0000

    functions

--- index.php
+++ index.php
@@ -4,33 +4,24 @@
   </head>
   <body>
     <?php 
-        $username = "Henrique Mazzoleni";
-        echo $username;
-        echo "<br>";
-        $current_user = $username;
-        echo $current_user;
-        echo "<br>";
-        $count = 14;
+    function longdate($timestamp)
+    {
+        $temp = date("l F jS Y", $timestamp);
+        return "The date is $temp";
+    }
+
+    function test()
+    {
+        static $count = 0;
         echo $count;
-        echo "<br>";
-        $count = 17.2;
-        echo $count;
-        echo "<br>";
-        $familia = array('Henrique', 'Annia', 'Gabriella');
-        echo $familia[1];
-        echo "<br>";
-        echo 2 + 5;
-        echo "<br>";
-        echo 7%2;
-        echo "<br>";
-        $count += 1;
-        ++$count;
-        echo $count;
-        echo "<br>";
-        echo 5 . 2;
-        echo "<br>";
-        echo $count.=2;
-        echo true == true and false;
+        $count++;
+    }
+    echo longdate(time());
+    echo "<br>";
+    test();
+    test();
+    test();
+    test();
     ?> 
   </body>
 </html>
