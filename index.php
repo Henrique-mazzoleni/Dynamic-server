5da2bc8230f53d3a9be4b756b4844189fea596a2
Henrique Mazzoleni <henrique.mazzoleni@gmail.com>
2021-11-19 11:56:08 +0000

    OOP

--- index.php
+++ index.php
@@ -5,14 +5,44 @@
   <body>
     <?php 
     // phpinfo();
-    if (function_exists("array_combine"))
+    // echo phpversion();
+    $object = new User('Henrique', 'senha');
+    print_r($object);
+    echo "<br />"; 
+    class User
     {
-        echo "Funtion Exists";
+        // public $name, $password;
+        const USER = 1;
+        const ADMIN = 2;
+        public function __construct($name, $password)
+        {
+            $this->name = $name;
+            $this->password = $password;
+            $this->permission = self::USER;
+        }
+
+        public function save_user()
+        {
+            echo "Save User code goes here";
+        }
     }
-    else
+
+    class Superuser extends User
     {
-        echo "Function does not exist - better write our own";
+        public function __construct($name, $password)
+        {
+            parent::__construct($name, $password);
+            $this->permission = self::ADMIN;
+        }
     }
+    $object->name = "Annia";
+    $object->password = "pass";
+    print_r($object);
+    echo "<br />";
+    $object->save_user();
+    echo "<br />";
+    $object2 = new Superuser('Gabriella', 'gagabubu');
+    print_r($object2);
     ?> 
   </body>
 </html>
